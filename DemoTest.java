42c7a571ae9cfa477ef3d219a1249460e3f74d0e
安思禹 <2582981227@qq.com>
2021-03-22 20:10:09 +0800

    再次添加

---
+++ DemoTest.java
@@ -0,0 +1,25 @@
+
+import org.junit.Test;
+
+
+public class DemoTest {
+
+    @Test
+    public void test2(){
+
+        for (int i=1;i<=9;i++){
+            for (int j=1;j<=10-i;j++){
+                System.out.print("       ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print(j+"*"+i+"="+(i*j)+" ");
+            }
+            System.out.println();
+        }
+		
+		sout
+
+    }
+
+
+}
